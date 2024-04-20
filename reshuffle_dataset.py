dab1c0511b1145ee497593d84f683b32b7ba1b27
Stan Sheludko <ssheludko@gmail.com>
2024-04-20 12:42:45 -0500

    Changed the way the json file is saved

--- reshuffle_dataset.py
+++ reshuffle_dataset.py
@@ -13,6 +13,17 @@ random.seed(563798)
 parser = ArgumentParser("Reshuffle the 2021 Agrivision Dataset to create labeled test set")
 parser.add_argument('input_dir', type=str, help='The root directory containing the dataset')
 
+labels_folder = {
+    'double_plant': 1,
+    'drydown': 2,
+    'endrow': 3,
+    'nutrient_deficiency': 4,
+    'planter_skip': 5,
+    'water': 6,
+    'waterway': 7,
+    'weed_cluster': 8
+}
+
 # change DATASET ROOT to your dataset path
 #DATASET_ROOT = "\\Users\\sshel\\OneDrive\\Documents\\OMSA\\CS 7643\\Project\\OMSA-CS7643-AgriVision\\data\\images_2021"
 
@@ -24,28 +35,51 @@ NEW_TRAIN_ROOT = os.path.join(DATASET_ROOT, 'new_train')
 NEW_VAL_ROOT = os.path.join(DATASET_ROOT, 'new_val')
 NEW_TEST_ROOT = os.path.join(DATASET_ROOT, 'new_test')
 
-print("Getting majority class labels for train data set")
-train_dict = get_major_class_label(TRAIN_ROOT)
-print("Getting majority class labels for val data set")
-val_dict = get_major_class_label(VAL_ROOT)
 
-merged_dict = {x:train_dict[x]+val_dict[x] for x in train_dict.keys()}
-print(Counter(merged_dict['majority_class_label']))
+### First check if this script was already run and a file mapping the old datasets to classes exists
+try:
+    with open("dataset_map.json", "r") as file:
+        in_dict = json.load(file)
+        merged_dict = {'basename':[],'origin_set':[],'majority_class_label':[]}
+        for i in in_dict.keys():
+            merged_dict['basename'].append(i)
+            merged_dict['origin_set'].append(in_dict[i]['origin_set'])
+            merged_dict['majority_class_label'].append(in_dict[i]['majority_class_label'])
+except:
+    print("Getting majority class labels for train data set")
+    train_dict = get_major_class_label(TRAIN_ROOT)
+    print("Getting majority class labels for val data set")
+    val_dict = get_major_class_label(VAL_ROOT)
+    merged_dict = {x:train_dict[x]+val_dict[x] for x in train_dict.keys()}
 
-with open("dataset_map.json", "w") as outfile:
-        json.dump(merged_dict, outfile)
+    out_dict = {merged_dict['basename'][i]:{'origin_set':merged_dict['origin_set'][i], 'majority_class_label':merged_dict['majority_class_label'][i]} for i in range(len(merged_dict['basename']))}    
 
+    with open("dataset_map.json", "w") as outfile:
+            json.dump(out_dict, outfile)
+
+reversed_keys = {v:k for k,v in labels_folder.items()}
+print("Distribution of classes:")
+c = Counter(merged_dict['majority_class_label'])
+print({x[0]:x[1] for x in sorted({reversed_keys.get(i,'background'):round(c[i]/sum(c.values()),3) for i in c}.items(), key = lambda x:-x[1])})
 
 X = merged_dict['basename']
 y = merged_dict['majority_class_label']
+
+### Uncomment if creating a small subset
+
+# l = merged_dict['basename']
+# i = list(enumerate(l))
+# random.shuffle(i)
+# idx, X = zip(*i[:2000]) 
+# y = [merged_dict['majority_class_label'][i] for i in idx]
 
 train_files, X_tt, _, y_tt = train_test_split(X, y, test_size=0.4, stratify=y)
 val_files, test_files, _, _ = train_test_split(X_tt,y_tt, test_size=0.5, stratify=y_tt)
 
 
-subfolders = ['boundaries','images\\nir','images\\rgb','labels\\double_plant','labels\\drydown',
-              'labels\\endrow','labels\\nutrient_deficiency','labels\\planter_skip','labels\\storm_damage',
-              'labels\\water','labels\\waterway','labels\\weed_cluster','masks']
+subfolders = ['boundaries','images/nir','images/rgb','labels/double_plant','labels/drydown',
+              'labels/endrow','labels/nutrient_deficiency','labels/planter_skip','labels/storm_damage',
+              'labels/water','labels/waterway','labels/weed_cluster','masks']
 
 
 def create_dirtree_without_files(src, dst):
--- src/utils/majority_class.py
+++ src/utils/majority_class.py
@@ -2,7 +2,7 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
-
+from collections import defaultdict
 
 labels_folder = {
     'double_plant': 1,
@@ -19,9 +19,9 @@ def img_basename(filename):
     return os.path.basename(os.path.splitext(filename)[0])
 
 def get_major_class_label(data_root):
-    basname = [img_basename(f) for f in os.listdir(os.path.join(data_root,'images\\rgb'))]
+    basename = [img_basename(f) for f in os.listdir(os.path.join(data_root,'images/rgb'))]
     dataset_mapping = {'basename':[], 'origin_set':[], 'majority_class_label':[] }
-    for file in tqdm(basname):
+    for file in tqdm(basename):
         dataset_mapping['basename'].append(file)
         dataset_mapping['origin_set'].append("train" if "train" in data_root else "val")    
         label_images = np.ndarray((9,512,512))
@@ -33,6 +33,6 @@ def get_major_class_label(data_root):
             label_images[label_index] = arr
         n_sum = np.argmax(np.sum(label_images,axis=(1,2)),axis=0)
         dataset_mapping['majority_class_label'].append(int(n_sum))
-   
+    
     return dataset_mapping
 
